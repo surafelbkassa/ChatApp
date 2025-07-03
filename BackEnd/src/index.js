7758686fa7388bf129f9d6184925f3ca92589029
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-07-04 02:31:39 +0300

    Initial commit with FrontEnd and BackEnd

---
+++ BackEnd/src/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import router from '../routes/auth.route.js'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT  = process.env.PORT;
+const app = express();
+
+app.use("/api/auth", router);
+
+app.listen(PORT, () => {
+    console.log("Server is running on port:" + PORT);  
+});
