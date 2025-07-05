14f7ab719bdae0e04fc37db03ef2e7dc9528e42e
Surafel Belachew <148704749+surafelbkassa@users.noreply.github.com>
2025-07-05 06:32:26 +0300

    functioning signup Login and Logout

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -41,9 +41,36 @@ export const signup = async (req,res) => {
         return res.status(500).json({message: "Internal server error"});
         
     }}
-export const login = (req,res) => {
-    res.send('login route');
+export const login = async (req,res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await prisma.users.findUnique({
+            where :{email}
+        })
+        if(!user){
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid){
+            return res.status(400).json({message: "Invalid email or password"});
+        }
+        generateToken(user.id, res);
+        return res.status(200).json({
+            id: user.id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        
+    }
 }
 export const logout = (req,res) => {
-    res.send('logout route');
+    try {
+        res.cookie('jwt','',{maxAge: 0});
+        res.status(200).json({"message":"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller: " + error.message);
+        res.status(500).json({"message": "Internal server error"})
+    }
 }
